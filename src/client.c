4fe4d61a68872c5dfefeeba294a2996ff2cd06ea
Wiesel <Pinpong10@mail.ru>
2024-12-22 07:33:49 +0300

    clean unnecessary commetnts and trash

--- src/client.c
+++ src/client.c
@@ -1,4 +1,3 @@
-// client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,7 +20,6 @@
 #define EVENT_SIZE  (sizeof(struct inotify_event))
 #define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
 
-// const int frequency = 10;
 char dir[BUFFER_SIZE] = {0};
 int freq = 0;
 
@@ -71,7 +69,7 @@ int main() {
         return -1;
     }
 
-    // Receive directory and frequency
+    // Прием дериктории и частоты от сервера
     int bytes_read = read(sock, buffer, BUFFER_SIZE);
     if (bytes_read <= 0) {
         printf("Failed to receive initial data\n");
@@ -117,7 +115,7 @@ void *inotify_watcher(void *arg) {
     }
 
     clock_t start_time = clock();           // Запоминаем начальное время
-    int timer_seconds = freq;          // Таймер на x секунд
+    int timer_seconds = freq;               // Таймер на x секунд
 
     while(1) {
         if (!((clock() - start_time) > timer_seconds * CLOCKS_PER_SEC)){
@@ -141,36 +139,17 @@ void *inotify_watcher(void *arg) {
                 const char *type_of_event;
 
                 if (event->mask & IN_CREATE || event->mask & IN_MOVED_TO ){
-                    // if (event->mask & IN_ISDIR) {
-                    //     printf("Directory created: %s\n", full_path);
-                    // } else {
-                    //     printf("File created: %s\n", full_path);
-                    // }
                     type_of_event = "create";
                 }
                 else if (event->mask & IN_DELETE || event->mask & IN_MOVED_FROM) {
-                    // if (event->mask & IN_ISDIR) {
-                    //     printf("Directory deleted: %s\n", full_path);
-                    // } else {
-                    //     printf("File deleted: %s\n", full_path);
-                    // }
                     type_of_event = "delete";
                 } else {
-                    // if (event->mask & IN_ISDIR) {
-                    //     printf("Directory modified: %s\n", full_path);
-                    // } else {
-                    //     printf("File modified: %s\n", full_path);
-                    // }
                     type_of_event = "unknown";
                 }
 
                 char sha256_buff[65];
                 // printf("path %s\n", full_path);
                 compute_sha256(full_path, sha256_buff);
-
-                // char file_name_d[255 + 1] = {0};  
-                // strncpy(file_name_d, event->name, event->len); 
-                // file_name_d[event->len] = '\0';  
 
                 snprintf(event_buffer, BUFFER_SIZE, "type_of_event: %s, file_name: %.255s, sha256: %s", 
                          type_of_event,
@@ -221,157 +200,6 @@ void compute_sha256(const char *str, char *outputBuffer) {
     }
     outputBuffer[64] = 0; // Завершаем строку нулевым символом
 }
-
-
-
-
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include <locale.h>
-// #include <time.h>
-// #include <pthread.h>
-// #include <unistd.h>
-
-// #include <sys/inotify.h>
-
-// #include <arpa/inet.h>
-// #include <openssl/ssl.h>
-// #include <msgpack.h>
-
-
-// void InitWatchDirectory(const char *path);
-// void SendMessageToServer();
-// void DirectoryWatcher(void);
-// void compute_sha256(const char *str, char *outputBuffer);
-// void create_json_message(const char *type_of_event, const char *file_name);
-
-// void *send_data_to_server(void *arg);
-// void *monitor_directory(void *arg);
-
-
-// // unsigned char directory_name[128] = "C:/DanyaMain/Projects_programming/C/C/ClientTCP/";  old
-// // const unsigned char directory_name[128] = "C:/DanyaMain/Projects_programming/C/appTCP/build/";
-// #define PATH_TO_WATCH "/programming/appTCP/appTCP"
-
-// int hDir = 0;
-// int flag_event = 0;
-
-// #define PORT 5555
-// #define BUFFER_SIZE 1024
-
-// int sock = 0;           // дескриптор сокета
-
-// char json_message[512];
-
-// #define MAX_EVENTS 16
-// #define EVENT_SIZE (sizeof(struct inotify_event))
-// #define BUF_LEN (MAX_EVENTS * (EVENT_SIZE + 16))
-
-// char *directory_to_watch = "/path/to/watch"; // Замените на нужную директорию
-
-
-// int main()
-// {
-//     SSL_library_init();
-//     SSL_load_error_strings();
-//     printf("OpenSSL Version: %s\n", OpenSSL_version(OPENSSL_VERSION));
-
-//     setlocale(LC_ALL, "Russian");           // для выводы в консоль кириллицы
-
-//     printf("I am CLIENT!\n");
-//     sleep(3);
-
-    
-//     //int sock;           // дескриптор сокета
-//     sock = socket(AF_INET, SOCK_STREAM, 0);
-//     if (sock < 0) {
-//         perror("socket failed");
-//         sleep(5);
-//         exit(EXIT_FAILURE);
-//     }
-
-//     struct sockaddr_in sa;
-//     memset(&sa, 0, sizeof(sa));
-//     sa.sin_family = AF_INET;
-//     sa.sin_port = htons(PORT);
-
-//     // sa.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-//     // sa.sin_addr.s_addr = htonl(INADDR_ANY);
-
-//     // Преобразование IP-адреса из строки в бинарный формат
-//     if (inet_pton(AF_INET, "127.0.0.1", &sa.sin_addr) <= 0) {
-//         printf("\nInvalid address/ Address not supported \n");
-//         sleep(5);
-//         return -1;
-//     }
-
-//     if(connect(sock, (struct sockaddr *)&sa, sizeof(sa)) < 0) {   // установка соединения с сервером
-//         printf("\nConnection Failed \n");
-//         sleep(5);
-//         return -1;
-//     }
-
-//     // InitWatchDirectory("C:/DanyaMain/Projects_programming/C/C/ClientTCP");
-//     // InitWatchDirectory("C:/DanyaMain/Projects_programming/C/appTCP/build/");
-
-
-//     // Создаем мьютекс и условную переменную
-//     pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-//     pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-
-//     // Создаем потоки
-//     pthread_t thread_send, thread_monitor;
-//     pthread_create(&thread_send, NULL, send_data_to_server, &sock);
-//     pthread_create(&thread_monitor, NULL, monitor_directory, &sock);
-
-//     // Ждем завершения потоков
-//     pthread_join(thread_send, NULL);
-//     pthread_join(thread_monitor, NULL);
-
-//     close(sock);
-
-//     // hMutex = CreateMutex(NULL, FALSE, NULL);
-//     // if (hMutex == NULL) {
-//     //     printf("Не удалось создать мьютекс\n");
-//     //     CloseHandle(hDir);
-//     //     return 1;
-//     // }
-
-//     // // Создаем поток для наблюдения за директорией
-//     // int hThreadWatcher = CreateThread(NULL, 0, DirectoryWatcher, NULL, 0, NULL);
-//     // if (hThreadWatcher == NULL) {
-//     //     printf("Не удалось создать поток наблюдателя\n");
-//     //     CloseHandle(hMutex);
-//     //     CloseHandle(hDir);
-//     //     return 1;
-//     // }
-
-//     // // Создаем поток для отправки сообщений
-//     // int hThreadMessendger = CreateThread(NULL, 0, SendMessageToServer, NULL, 0, NULL);
-//     // if (hThreadMessendger == NULL) {
-//     //     printf("Не удалось создать поток\n");
-//     //     CloseHandle(hThreadWatcher);
-//     //     CloseHandle(hMutex);
-//     //     CloseHandle(hDir);
-//     //     return 1;
-//     // }
-
-//     // // Ждем завершения обоих потоков (в реальном приложении это может быть бесконечный цикл или условие выхода)
-//     // WaitForSingleObject(hThreadWatcher, INFINITE);
-//     // WaitForSingleObject(hThreadMessendger, INFINITE);
-
-//     // // Освобождаем ресурсы
-//     // CloseHandle(hThreadWatcher);
-//     // CloseHandle(hThreadMessendger);
-//     // CloseHandle(hMutex);
-//     // CloseHandle(hDir);
-
-//     printf("Закрытие программы");
-
-//     return 0;
-// }
 
 
 
--- src/server.c
+++ src/server.c
@@ -17,7 +17,6 @@
 const char *filename = "log.txt";               // Имя файла для записи
 
 
-// void handle_client(int client_sock) ;
 void writeArrayToFile(const char *filename, char *array, int size);
 void sigterm_handler(int sig);
 
@@ -69,16 +68,10 @@ int main(int argc, char *argv[]) {
 
     printf("Server listening on port %d...\n", PORT);
 
-    //  // Запуск клиента (компиляция и выполнение) в отдельном процессе.
-    //  if (system("./client &") != 0) { // Запускаем client в фоне (&)
-    //     perror("Failed to run client");
-    //     exit(EXIT_FAILURE);
-    // }
 
-    // Forking to run client in a new process
+    // Запуск клиента, как отдельный процесс
     pid_t pid = fork();
     if (pid == 0) {
-        // Child process
         execl("./client", "client", NULL);
         perror("execl");
         exit(EXIT_FAILURE);
@@ -96,7 +89,6 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // handle_client(new_socket);   
     char buffer[BUFFER_SIZE] = {0};
     snprintf(buffer, BUFFER_SIZE, "%s %d", argv[1], atoi(argv[2]));
     send(new_socket, buffer, strlen(buffer), 0);
@@ -107,39 +99,13 @@ int main(int argc, char *argv[]) {
 
         while(recv(new_socket, json_message, sizeof(json_message), 0) > 0 )
         {
-            //int size_buf = sizeof(buf) / sizeof(buf[0]);    // Размер массива
             int size_buf = sizeof(json_message) / sizeof(json_message[0]);    // Размер массива
             writeArrayToFile(filename, json_message, size_buf);
 
             printf("\n%s\n", json_message);
             printf("\n");
 
-
-            /*char nm[20] = "popopopopo\0";
-            send(client_socket, nm, sizeof(nm), 0);*/
-
         }
-
-    // // Получаем текущий путь к директории, где находится server.c (предполагаем, что это и есть проект)
-    //  if (getcwd(project_path, sizeof(project_path)) == NULL) {
-    //     perror("getcwd() error");
-    //     exit(EXIT_FAILURE);
-    //  }
-    // // 6. Отправка пути проекта клиенту
-    // if (send(new_socket, project_path, strlen(project_path), 0) < 0)
-    // {
-    //      perror("send failed");
-    //      exit(EXIT_FAILURE);
-    // }
-    //  printf("Project path sent: %s\n", project_path);
-
-    // // 7. Чтение ответа от клиента
-    // if (recv(new_socket, buffer, BUFFER_SIZE, 0) < 0)
-    // {
-    //     perror("recv failed");
-    //      exit(EXIT_FAILURE);
-    // }
-    // printf("Client response: %s\n", buffer);
 
     // 8. Закрытие соединения и сокета
     close(new_socket);
@@ -156,198 +122,6 @@ void sigterm_handler(int sig) {
     exit(0);
 }
 
-void handle_client(int client_sock) {
-    // char buffer[BUFFER_SIZE] = {0};
-    // msgpack_packer pk;
-    // msgpack_sbuffer sbuf;
-
-    // msgpack_sbuffer_init(&sbuf);
-    // msgpack_packer_init(&pk, &sbuf, msgpack_sbuffer_write);
-
-    // while(1) {
-    //     int bytes_read = read(client_sock, buffer, sizeof(buffer));
-    //     if (bytes_read <= 0) break;
-
-    //     msgpack_unpacked msg;
-    //     msgpack_unpacked_init(&msg);
-    //     if (msgpack_unpack_next(&msg, buffer, bytes_read, NULL) != MSGPACK_UNPACK_SUCCESS) {
-    //         printf("Failed to unpack message\n");
-    //         continue;
-    //     }
-
-    //     if (msg.data.type != MSGPACK_OBJECT_MAP) {
-    //         printf("Invalid message format\n");
-    //         continue;
-    //     }
-
-    //     msgpack_object_kv* kv = msg.data.via.map.ptr;
-    //     printf("Event: %s, Path: %s\n", kv[0].val.via.str.ptr, kv[1].val.via.str.ptr);
-
-    //     msgpack_sbuffer_destroy(&sbuf);
-    //     msgpack_sbuffer_init(&sbuf);
-    //     msgpack_unpacked_destroy(&msg);
-    // }
-
-    // close(client_sock);
-}
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <locale.h>
-// #include <string.h>
-// #include <stdbool.h>
-// #include <time.h>
-
-// #include <sys/socket.h>
-// // #include <netinet/in.h>
-// #include <arpa/inet.h>
-// #include <sys/types.h>
-// #include <sys/wait.h>
-// #include <signal.h>
-
-
-// #define PORT 5555
-// #define BUFFER_SIZE 1024
-// pid_t client_pid = -1;
-
-// void writeArrayToFile(const char *filename, char *array, int size);
-// bool LaunchClient(int server_fd, int new_socket); 
-
-// void signal_handler(int signum);
-
-// const char *filename = "log.txt";               // Имя файла для записи
-
-// int connection_counter = 0;
-
-// int main(/*int argc, char *argv[]*/ )
-// {
-//     setlocale(LC_ALL, "Russian");           // для выводы в консоль кириллицы
-
-//     // if (argc != 3) {
-//     //     printf("Error arguments counter\n");
-//     //     sleep(5);
-//     //     return EXIT_FAILURE;
-//     // }
-//     // if (strcmp(argv[1], "server") == 0) {
-//     //     printf("correct start - server\n");
-//     // } else {
-//     //     printf("Error arguments counter\n");
-//     //     return EXIT_FAILURE;
-//     // }
-
-//     printf("I am SERVER!\n");
-
-//     signal(SIGINT, signal_handler);
-//     signal(SIGTERM, signal_handler);
-
-//     int server_socket, new_socket;           // дескриптор сокета
-//     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-//     if (server_socket == 0) {
-//         perror("socket failed");
-//         sleep(5);
-//         exit(EXIT_FAILURE);
-//     }
-
-//     struct sockaddr_in socket_addr;
-//     int addrlen = sizeof(socket_addr);
-//     memset(&socket_addr, 0, sizeof(socket_addr));
-
-//     socket_addr.sin_family = AF_INET;
-//     socket_addr.sin_port = htons(PORT);
-//     socket_addr.sin_addr.s_addr = INADDR_ANY;
-
-//     if (bind(server_socket, (struct sockaddr *)&socket_addr, sizeof(socket_addr)) < 0) {
-//         perror("bind failed");
-//         sleep(5);
-//         close(server_socket);
-//         exit(EXIT_FAILURE);
-//     }
-
-//     // listen(s, 100);  // 100 - размер очередиы
-//     // 3. Прослушивание входящих соединений
-//     if (listen(server_socket, 3) < 0) {
-//         perror("listen failed");
-//         sleep(5);
-//         close(server_socket);
-//         exit(EXIT_FAILURE);
-//     }
-
-//     printf("Server is listening on port %d\n", PORT);
-
-//     char json_message[512];
-//     memset(json_message, 0, sizeof(json_message));
-
-//     int client_socket;
-//     // SOCKADDR_IN client_addr;
-//     // int client_addr_size = sizeof(client_addr);
-
-//     // Запускаем клиента
-//     // if (!LaunchClient()) {
-//     //     printf("Failed to launch client.\n");
-//     //     Sleep(5000);
-//     //     return 1; // Или обработать ошибку по-другому
-//     // }
-//     // LaunchClient();
-
-//         // if (LaunchClient(server_socket, client_socket) == 1) {
-//         //     printf("Continue\n");
-//         //     sleep(2);
-//         //     // continue;
-//         // }
-
-//     // while(1)
-//     while(client_socket = accept(server_socket, (struct sockaddr *)&socket_addr, &addrlen))
-//     {
-//         // if ((client_socket = accept(s, (struct sockaddr *)&socket_addr, &addrlen)) < 0) {
-//         //     perror("accept failed");
-//         //     close(s);
-//         //     // sleep(5);
-//         //     exit(EXIT_FAILURE);
-//         // }
-
-//         if (client_socket < 0){
-//             printf("Continue socket\n");
-//             // sleep(3);
-//             continue;
-//         }
-
-//         if (LaunchClient(server_socket, client_socket) == 1) {
-//             printf("Continue\n");
-//             sleep(2);
-//             continue;
-//         }
-
-
-//         printf("Connect OK");
-//         printf(" - number = %d\n", ++connection_counter);
-
-
-//         while(recv(client_socket, json_message, sizeof(json_message), 0) > 0 )
-//         {
-//             //int size_buf = sizeof(buf) / sizeof(buf[0]);    // Размер массива
-//             int size_buf = sizeof(json_message) / sizeof(json_message[0]);    // Размер массива
-//             writeArrayToFile(filename, json_message, size_buf);
-
-//             printf("\n%s\n", json_message);
-//             printf("\n");
-
-
-//             /*char nm[20] = "popopopopo\0";
-//             send(client_socket, nm, sizeof(nm), 0);*/
-
-//         }
-//         // close(client_socket);
-//         // close(server_socket);
-//     }
-//     printf("Connect wtf\n");
-
-//     close(client_socket);
-//     close(server_socket);
-
-//     return 0;
-// }
-
 
 void writeArrayToFile(const char *filename, char *array, int size)
 {
@@ -357,7 +131,6 @@ void writeArrayToFile(const char *filename, char *array, int size)
         perror("Ошибка открытия файла");
         exit(EXIT_FAILURE);
     }
-
 
     // Получаем текущее время
     time_t now = time(NULL);
@@ -383,8 +156,6 @@ void writeArrayToFile(const char *filename, char *array, int size)
         exit(EXIT_FAILURE);
     }
 
-
-
     // Проверяем, пуст ли файл
     fseek(file, 0, SEEK_END);       // Перемещаем указатель в конец файла
     long fileSize = ftell(file);    // Узнаём размер файла
@@ -395,18 +166,10 @@ void writeArrayToFile(const char *filename, char *array, int size)
     // Записываем метку времени в файл
     fprintf(file, "[%s] ", timestamp);
 
-    // Записываем элементы массива в файл через пробел
-    /*for (int i = 0; i < size; i++) {
-        fprintf(file, "%d", array[i]);
-        if (i < size - 1) {
-            fprintf(file, " "); // Добавляем пробел между элементами
-        }
-    }*/
     // Записываем массив в файл
     if (fputs(array, file) == EOF) {
         perror("Ошибка записи в файл");
         fclose(file);
-        //return 1;
     }
     fprintf(file, "\n");
 
